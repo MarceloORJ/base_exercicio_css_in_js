48ff7dda11ff36197903f25d0796cf58e2b1f812
Marcelo Oliveira <marcelo@exemplo.com>
2024-01-30 13:07:01 -0300

    Finalizacao exercicio

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import { EstiloGlobal, Container } from './styles'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
